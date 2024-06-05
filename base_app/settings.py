90ddf490edbd348415ec06e888fea4371268e714
David Hoffmann <adavidho@amazon.com>
2024-06-05 10:40:38 +0200

    Database setup

--- base_app/settings.py
+++ base_app/settings.py
@@ -75,18 +75,6 @@ WSGI_APPLICATION = "base_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'flightx_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'superuser',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
